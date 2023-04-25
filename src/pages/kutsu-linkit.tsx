86b766ac1e48f9fd45a6e9a08d72c19cbafe97b3
Ekku Sipilä <ekku.eki@gmail.com>
2023-04-25 16:33:02 +0300

    Add copy buttons to kutsu-linkit page

--- src/pages/kutsu-linkit.tsx
+++ src/pages/kutsu-linkit.tsx
@@ -1,25 +1,51 @@
 import * as React from "react";
-import { ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import { IconButton, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 import { PageProps } from "gatsby";
+import { MdOutlineContentCopy } from "react-icons/md";
+import { BsCheckLg } from "react-icons/bs";
 import PageWrapper from "../components/PageWrapper";
 
 import data from "../data.json";
 
-function InvitationLink(props: { id: string; label: string; origin: string }) {
-  const { id, label, origin } = props;
+function InvitationLink(props: {
+  id: string;
+  label: string;
+  origin: string;
+  onClickCopy: (href: string, id: string) => void;
+  copiedId: string;
+}) {
+  const { id, label, origin, onClickCopy, copiedId } = props;
   const href = `${origin}/${id}`;
+
   return (
     <ListItem>
       <Text as="span">{label}: </Text>
       <Text as="a" href={href}>
         {href}
       </Text>
+      <IconButton
+        variant="ghost"
+        _hover={{
+          bg: "none",
+        }}
+        onClick={() => onClickCopy(href, id)}
+        isDisabled={id === copiedId}
+        icon={id === copiedId ? <BsCheckLg /> : <MdOutlineContentCopy />}
+        aria-label="Copy href to clipboard"
+      />
     </ListItem>
   );
 }
 
 function InvitationLinksPage(props: PageProps) {
   const { location } = props;
+
+  const [copiedId, setCopiedId] = React.useState("");
+
+  const copyHrefToClipboard = async (href: string, id: string) => {
+    await navigator.clipboard.writeText(href);
+    setCopiedId(id);
+  };
 
   return (
     <PageWrapper p="3">
@@ -28,7 +54,13 @@ function InvitationLinksPage(props: PageProps) {
       </Text>
       <UnorderedList>
         {data.map((it) => (
-          <InvitationLink key={it.id} origin={location.origin} {...it} />
+          <InvitationLink
+            key={it.id}
+            copiedId={copiedId}
+            onClickCopy={copyHrefToClipboard}
+            origin={location.origin}
+            {...it}
+          />
         ))}
       </UnorderedList>
     </PageWrapper>
